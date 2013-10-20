3cf13ee6d01cb227201ac477b2dffbebe3b43564
mpescarmona <mpescarmona@gmail.com>
2013-10-20 01:06:18 -0300

    Change application name from myApp to testApp. Adding new partial html pages for contacts.

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,17 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('testApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'testApp.filters',
+  'testApp.services',
+  'testApp.directives',
+  'testApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/contacts', {templateUrl: 'partials/partialContacts.html', controller: 'ContactController'});
+  $routeProvider.when('/contacts/:contactId', {templateUrl: 'partials/partialContactDetails.html', controller: 'ContactDetailController'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,45 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
+angular.module('testApp.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
   .controller('MyCtrl2', [function() {
 
+  }])
+  .controller('ContactController', ['$scope', function($scope) {
+    $scope.contacts = [
+      {'id':1,
+      'name': 'Pedro Navaja',
+      'email': 'pnavaja@blades.com'},
+      {'id':2,
+      'name': 'Piano Man',
+      'email': 'pianoman@billyjoel.com'},
+      {'id':3,
+      'name': 'Mr. Jones',
+      'email': 'mrjones@charlygarcia.com'}
+    ];
+  }])
+  .controller('ContactDetailController', ['$scope', '$routeParams', function($scope, $routeParams) {
+    switch($routeParams.contactId)
+    {
+      case '1':
+        $scope.contact = {'id':1,
+                          'name': 'Pedro Navaja',
+                          'email': 'pnavaja@blades.com'};
+        break;
+      case '2':
+        $scope.contact = {'id':2,
+                          'name': 'Piano Man',
+                          'email': 'pianoman@billyjoel.com'};
+        break;
+      case '3':
+        $scope.contact = {'id':3,
+                          'name': 'Mr. Jones',
+                          'email': 'mrjones@charlygarcia.com'};
+        break;
+      default:
+        $scope.contact = {};
+    }
   }]);
